37ce149df0c2e5ef62c2449750b70d9a35533129
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-17 22:31:57 +0900

    Add index, login

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -2,13 +2,23 @@
 
 class AuthController {
   // Show Page
-  async index ({ view }) {
-    return view.render('auth-index')
+  async index ({ view, auth }) {
+    try {
+      await auth.check()
+    } catch (error) {
+      console.log(error)
+      return view.render('auth-index')
+    }
   }
 
   // Do login
-  async login () {
+  async login ({ request, auth, response }) {
+    const { email, password } = request.all()
+    console.log(email, password)
 
+    await auth.attempt(email, password)
+
+    return response.route('StudyController.index')
   }
 }
 
