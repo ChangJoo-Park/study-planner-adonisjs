45744533d48514c1b50b04060345feb47b00127d
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-19 23:23:38 +0900

    Add  store method to study controller

--- app/Controllers/Http/StudyController.js
+++ app/Controllers/Http/StudyController.js
@@ -12,7 +12,19 @@ class StudyController {
     return view.render('studies/create')
   }
 
-  async store () {
+  async store ({ request, response, view, auth }) {
+    const { name, description } = await request.only(['name', 'description'])
+    const newStudy = await new Study()
+    newStudy.name = name
+    newStudy.description = description
+
+    try {
+      const saved = await auth.user.studies().save(newStudy)
+      return response.route('StudyController.index')
+    } catch (e) {
+      console.log('ERROR')
+      console.log(e)
+    }
   }
 
   async show () {
