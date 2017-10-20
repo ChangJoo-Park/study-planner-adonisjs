f2d15bd400e49d80ff189e26daf59bf91fd692f4
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-20 23:13:03 +0900

    Add methods for iteration controller

--- app/Controllers/Http/IterationController.js
+++ app/Controllers/Http/IterationController.js
@@ -1,6 +1,25 @@
 'use strict'
 
 class IterationController {
+  async index () {
+
+  }
+
+  async store () {
+
+  }
+
+  async update () {
+
+  }
+
+  async update () {
+    
+  }
+    
+  async delete () {
+    
+  }
 }
 
 module.exports = IterationController
