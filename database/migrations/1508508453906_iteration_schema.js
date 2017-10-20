111d472466e737f46b31d3ba935814017e0941ea
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-20 23:19:38 +0900

    Add iteration schema

--- database/migrations/1508508453906_iteration_schema.js
+++ database/migrations/1508508453906_iteration_schema.js
@@ -6,6 +6,11 @@ class IterationSchema extends Schema {
   up () {
     this.create('iterations', (table) => {
       table.increments()
+      table.integer('user_id').references('id').inTable('users')
+      table.integer('study_id').references('id').inTable('studies')
+      table.string('name').notNullable()
+      table.date('started_at').notNullable()
+      table.date('ended_at').notNullable()
       table.timestamps()
     })
   }
