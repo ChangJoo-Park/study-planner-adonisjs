baa61461cee4c4cec998214f48320e33d91971f1
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-21 14:13:54 +0900

    Create iteration with user_id, study_id

--- app/Controllers/Http/IterationController.js
+++ app/Controllers/Http/IterationController.js
@@ -1,8 +1,27 @@
 'use strict'
 
+const Iteration = use('App/Models/Iteration')
+
 class IterationController {
-  async store () {
-    console.log('iteration store')
+  async store ({ request, response, params, auth }) {
+    const studyId = params['study_id']
+    const { name, started_at, ended_at } = request.only(['name', 'started_at', 'ended_at'])
+    const iteration = new Iteration()
+    iteration.user_id = auth.user.id
+    iteration.study_id = studyId
+    iteration.name = name
+    iteration.started_at = started_at
+    iteration.ended_at = ended_at
+
+    try  {
+      const saved = await iteration.save()
+      if (saved) {
+        // TODO: Render with iteration
+      }
+    } catch (e) {
+      console.log(e)
+    }
+
   }
 
   async update () {
